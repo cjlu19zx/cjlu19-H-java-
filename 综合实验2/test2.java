167dada6ee053d1252ef0357b844cd39c8a80520
cjlu19zx <58368775+cjlu19zx@users.noreply.github.com>
2020-06-23 21:44:11 +0800

    新增中文注释

--- 综合实验2/test2.java
+++ 综合实验2/test2.java
@@ -11,6 +11,14 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+/**
+ * 由Windows Builder构造
+ * @author zx
+ * 程序功能：
+ * 显示一个对话框，输出班级，姓名，学号
+ *
+ */
+
 public class test2 extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
@@ -39,17 +47,20 @@ public class test2 extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		{
+			// 显示班级，学号，姓名
 			JLabel lblNewLabel = new JLabel("19\u8BA1\u7B97\u673AH1\u73ED\uFF0C\u5B66\u53F733\uFF0C\u5F20\u9704");
-			lblNewLabel.setFont(new Font("ËÎÌå", Font.PLAIN, 20));
+			lblNewLabel.setFont(new Font("宋体", Font.PLAIN, 20));
 			lblNewLabel.setBounds(40, 13, 292, 62);
 			contentPanel.add(lblNewLabel);
 		}
 		
+		// 显示内容“点击右上角关闭”
 		JLabel lblNewLabel_1 = new JLabel("\u70B9\u51FB\u53F3\u4E0A\u89D2\u5173\u95ED");
 		lblNewLabel_1.setBounds(242, 78, 122, 18);
 		contentPanel.add(lblNewLabel_1);
 		lblNewLabel_1.setVisible(false);
 		
+		// “确定按钮”
 		JButton btnNewButton = new JButton("\u786E\u5B9A");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
